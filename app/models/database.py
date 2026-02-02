7ef62e57d79769af9bf06e2cc980114f7b6e4747
Novruz <lektor7@inbox.ru>
2026-02-02 21:54:08 +0300

    fix

--- app/models/database.py
+++ app/models/database.py
@@ -1,4 +1,6 @@
-
+"""
+Конфигурация подключения к базе и создание базы моделей SQLAlchemy
+"""
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
